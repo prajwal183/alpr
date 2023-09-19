f9e10b881610249e08c290a30ff817639b780b10
Prajwal Kumar <80627601+prajwal183@users.noreply.github.com>
2023-09-19 10:12:23 +0530

    Create licensePlateSizes.js

---
+++ licensePlateSizes.js
@@ -0,0 +1,22 @@
+exports.licensePlates = {
+  cab: {
+    aspectRatio: {
+      min: 2,
+      max: 6,
+    },
+    area: {
+      min: 4500,
+      max: 30000,
+    },
+  },
+  auto: {
+    aspectRatio: {
+      min: 1,
+      max: 2,
+    },
+    area: {
+      min: 9000,
+      max: 30000,
+    },
+  },
+};
